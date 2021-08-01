e7757ab54c4d3c55854132664d5a8e86698b1588
j0suesmith1 <88245151+j0suesmith1@users.noreply.github.com>
2021-08-01 11:18:37 -0500

    Update index.js

--- index.js
+++ index.js
@@ -93,7 +93,7 @@ const vcard = 'BEGIN:VCARD\n' // Tarjeta de contacto
             + 'END:VCARD'
 /******FIN DE ENTRADA VCARD******/
 
-prefix = ''
+prefix = '/'
 blocked = []
 
 /******CONFIGURACION DE CARGA******/
